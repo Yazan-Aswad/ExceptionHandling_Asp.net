673c46dbb1a464d5f7d8cec5917e49f17efb7ebc
Yazan-Aswad <ityazan1@gmail.com>
2024-05-17 10:21:00 +0300

    ExceptionHandling ASP.Net

---
+++ ExceptionHandling/ExceptionHandling/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static ExceptionHandling.MvcApplication;
+
+namespace ExceptionHandling.Controllers
+{
+    [MyExceptionHandler]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string msg = null;
+            ViewBag.Message = msg.Length; // this will throw an exception
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HandleError]
+        [HandleError(ExceptionType = typeof(NullReferenceException), View = "~/Views/shared/Handleerrorattribute.cshtml")]
+        public ActionResult Contact()
+        {
+            string msg = null;
+            ViewBag.Message = msg.Length; // this will throw an exception
+
+            return View();
+        }   
+        
+        
+        public ActionResult Contact2()
+        {
+            string msg = null;
+            ViewBag.Message = msg.Length; // this will throw an exception
+
+            return View();
+        }
+
+        //protected override void OnException(ExceptionContext filterContext)
+        //{
+        //    filterContext.ExceptionHandled = true;
+
+        //    //Log the error!!
+
+        //    //Redirect to action
+        //    //filterContext.Result = RedirectToAction("Error", "InternalError");
+
+        //    // OR return specific view
+        //    filterContext.Result = new ViewResult
+        //    {
+        //        ViewName = "~/Views/shared/ControllerOnException .cshtml"
+        //    };
+        //}
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ExceptionHandling
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        public class MyExceptionHandler : HandleErrorAttribute
+        {
+            public override void OnException(ExceptionContext filterContext)
+            {
+                Exception e = filterContext.Exception;
+                filterContext.ExceptionHandled = true;
+                filterContext.Result = new ViewResult()
+                {
+                    ViewName = "MyExceptionHandlerinGlobalasax"
+                };
+            }
+        }
+
+    }
+}
